f9e3de564586493b73d30572fcb09e9183804433
Joseph Skrzysowski <josephskrzysowski@gmail.com>
2017-08-01 12:00:38 -0400

    fine tuning

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'baseketBall',
+    password: 'Oliver',
     database: 'tutors_db'
 });
 }
